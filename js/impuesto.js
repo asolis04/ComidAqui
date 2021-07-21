62cb1c7a39eecb27f3c7d7f56831aa476416b471
asolis04 <anita4.7@hotmail.com>
2021-07-20 20:12:46 -0600

    Impuesto

--- js/impuesto.js
+++ js/impuesto.js
@@ -16,3 +16,40 @@ btnConfiImpuesto.addEventListener('click', () => {
     listarImpuesto.classList.add('ocultar');
     confImpuesto.classList.remove('ocultar');
 });
+
+
+
+const camposRequeridosInfo = document.querySelectorAll('#frm-impuesto :required');
+const btnInfo = document.querySelector('#btnRegistrar');
+
+
+const validarInfo = () => {
+    let error = false;
+
+    camposRequeridosInfo.forEach(campo => {
+        if (campo.value == '') {
+            error = true;
+            campo.classList.add('error');
+        } else {
+            campo.classList.remove('error');
+        }
+    });
+    if (error == true) {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Faltan Datos requeridos!',
+            text: 'Por Favor ingrese todos los datos'
+        });
+    } else {
+        Swal.fire({
+            icon: 'success',
+            title: 'Información Enviada!',
+            text: 'Su información se envío correctamente.'
+        });
+
+    }
+
+    camposRequeridosInfo.values = "";
+};
+
+btnInfo.addEventListener('click', validarInfo);
